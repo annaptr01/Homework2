eab05c306f6fe66571d39b6af0b59766f6d6c23c
anna <annetakaulitz@mail.ru>
2023-04-26 23:43:49 +0300

    finish editing homework

--- Program.cs
+++ Program.cs
@@ -23,27 +23,46 @@ else
 */
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
 //или сообщает, что третьей цифры нет.
+//есть одна погрешность, я сделала только для трехзначных чисел код, как для 4х значных 
+//и более сделать код, чтоб искал только 3 цифру, я не знаю 
 /*
-void Digit(int user_num)
+int Digit1(int user_num)
 {
-    int digit1 = user_num / 100;
-    int digit2 = user_num / 10;
-    int digit3 = user_num % 10;
+    int digit1 = user_num / 100; //456 = 4
+    int digit2 = user_num / 10; //456 = 5
+    int digit3 = user_num % 10; //456 = 6
     int result = digit3;
-    if(user_num > 99 && user_num < 1000)
-    {
-        Console.WriteLine("The 3rd digit of your number " + user_num + " is: " + result);
+    Console.WriteLine("The 3rd digit of your number " + user_num + " is: " + result);
+    return result;
+}
+Console.WriteLine("Enter your number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
 
+    if(user_number > 99 && user_number < 1000 || user_number < -99 && user_number > - 1000)
+    {
+       Digit1(user_number);
     }
     else
     {
-        Console.WriteLine("There is no 3 digit number!");
+        Console.WriteLine("There is too large number!");
     }
-}
-Console.WriteLine("Enter your number: ");
-int user_num = Convert.ToInt32(Console.ReadLine());
 */
 
 //Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
 //и проверяет, является ли этот день выходным.
 
+void Say(number)
+{
+    if (number >= 6)
+    {
+        Console.WriteLine("YES! Number of the day: " + number + " is day off");
+    }
+    else
+    {
+        Console.WriteLine("NO! Number of the day: " + number + " is not day off");
+    }
+}
+
+Console.WriteLine("Enter your number of the day: ");
+int new_number = Convert.ToInt32(Console.ReadLine());
+Say(new_number);
